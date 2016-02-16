d8fd123e295ecdbde6881d9263c64bb6ebd696e6
Vin <info@webkleur.com>
2016-02-16 19:36:12 +0100

    save all xml data to entity. added strc & data sql. /easy /flexxx

--- README.md
+++ README.md
@@ -7,8 +7,9 @@ Test Resengo API
 * composer install
 * ->set up db
 * php bin/console doctrine:schema:update --force
-
-run /resengo_project/start_resengo.bat
+* run /resengo_project/start_resengo.bat
+* browse http://localhost:8000/
+* done
 
 * Quicklinks
 * https://www.resengo.com/Code/API/?APIClientID=1020139&Call=RN_LASTMINUTES&APISECRET=NLo4bkphb3dqUWGmE8VUcxqwQowwTlwcrsxFJqg7FpTdJSNWjR&RN_CompanyID=292493
--- resengo_project/src/AppBundle/Controller/DefaultController.php
+++ resengo_project/src/AppBundle/Controller/DefaultController.php
@@ -6,22 +6,39 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Timeslots;
 
 class DefaultController extends Controller
 {
     /**
-	 * @Route("/", name="homepage")
+     * @Route("/", name="homepage")
      * @Route("/resengo", name="homepage")
      */
     public function indexAction(Request $request)
     {
         $mypix = simplexml_load_file('https://www.resengo.com/Code/API/?APIClientID=1020139&Call=RN_LASTMINUTES&APISECRET=NLo4bkphb3dqUWGmE8VUcxqwQowwTlwcrsxFJqg7FpTdJSNWjR&RN_CompanyID=292493');
         foreach ($mypix->RECORD as $p):
-            $start_time=$p->StartTime;
-            $description=$p->Description;
-            $reservation_link= $p->ReservationURL;
-          echo "<a href='".$reservation_link."''> start time:".$start_time." - desciption: ".$description."</a> end <br>";
+            $start_time = $p->StartTime;
+            $description = $p->Description;
+            $reservation_link = $p->ReservationURL;
+            //echo for no reason .. lol -> work in progress
+            echo "'" . $reservation_link . "''> start time:" . $start_time . " - desciption: " . $description . " <br>";
+            $timeslot = new Timeslots();
+            $timeslot->setLastminuteid($p->LastMinuteID);
+            $timeslot->setCompanyid($p->CompanyID);
+            $timeslot->setStarttime($p->StartTime);
+            $timeslot->setEndtime($p->EndTime);
+            $timeslot->setDescription($p->Description);
+            $timeslot->setCost($p->Cost);
+            $timeslot->setOriginalcost($p->OriginalCost);
+            $timeslot->setInsertdate($p->InsertDate);
+            $timeslot->setDeleted($p->Deleted);
+            $timeslot->setReservationurl($p->ReservationURL);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($timeslot);
+            $em->flush();
         endforeach;
+        echo "<br>done all flushed in db<br>";
         return new Response();
     }
 }
