8b9188de0b068c976b43fa5574356e6f9f7791af
Vin <info@webkleur.com>
2016-02-16 18:27:36 +0100

    get data from xml

--- resengo_project/src/AppBundle/Controller/DefaultController.php
+++ resengo_project/src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -14,6 +15,13 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-       return "hello";
+        $mypix = simplexml_load_file('https://www.resengo.com/Code/API/?APIClientID=1020139&Call=RN_LASTMINUTES&APISECRET=NLo4bkphb3dqUWGmE8VUcxqwQowwTlwcrsxFJqg7FpTdJSNWjR&RN_CompanyID=292493');
+        foreach ($mypix->RECORD as $p):
+            $start_time=$p->StartTime;
+            $description=$p->Description;
+            $reservation_link= $p->ReservationURL;
+          echo "<a href='".$reservation_link."''> start time:".$start_time." - desciption: ".$description."</a> end <br>";
+        endforeach;
+        return new Response();
     }
 }
