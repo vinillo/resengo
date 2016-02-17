24eedccb3aeed42bacbe9c68172264795a958323
Vin <info@webkleur.com>
2016-02-17 13:48:50 +0100

    few logical fixes. ->starttime etc. should work

--- backups/sql/timeslot_struc_v1.sql
+++ backups/sql/timeslot_struc_v1.sql
@@ -1,20 +1,38 @@
 /*
 SQLyog Ultimate v11.42 (64 bit)
-MySQL - 5.6.20-log 
+MySQL - 5.6.20-log : Database - resengo
 *********************************************************************
-*/
+*/
+
 /*!40101 SET NAMES utf8 */;
 
-create table `timeslots` (
-	`id` int (11),
-	`lastminute_id` int (11),
-	`company_id` int (11),
-	`start_time` varchar (192),
-	`$endtime` varchar (192),
-	`description` varchar (768),
-	`cost` int (11),
-	`integer` varchar (33),
-	`insert_date` varchar (192),
-	`deleted` varchar (96),
-	`reservation_url` varchar (192)
-); 
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `timeslots` */
+
+DROP TABLE IF EXISTS `timeslots`;
+
+CREATE TABLE `timeslots` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lastminute_id` int(11) NOT NULL,
+  `company_id` int(11) NOT NULL,
+  `start_time` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `end_time` varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `cost` int(11) NOT NULL,
+  `insert_date` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `deleted` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `reservation_url` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `orginal_cost` int(11) NOT NULL,
+  `bool_verwijderd` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=527 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- resengo_project/src/AppBundle/Controller/DefaultController.php
+++ resengo_project/src/AppBundle/Controller/DefaultController.php
@@ -2,25 +2,29 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use AppBundle\Entity\Timeslots;
 
 class DefaultController extends Controller
 {
     private $xml_url;
 
+
     /**
      * @Route("/")
      * @Route("/resengo")
      */
 
+
     public function indexAction(Request $request)
-    {   //config
+    {
+
+        //config
         $this->setXmlUrl("https://www.resengo.com/Code/API/?APIClientID=1020139&Call=RN_LASTMINUTES&APISECRET=NLo4bkphb3dqUWGmE8VUcxqwQowwTlwcrsxFJqg7FpTdJSNWjR&RN_CompanyID=292493");
         //end config
-
+        date_default_timezone_set('Europe/Amsterdam');
         if (!$this->checkIfXmlExist()):
             throw $this->createNotFoundException(
                 'Fatal Error: xml not found '
@@ -55,14 +59,41 @@ class DefaultController extends Controller
                         $timeslot->setBoolVerwijderd(0);
                     endif;
                     if (strtotime($p->StartTime) < time()):
-                        $timeslot->setBoolVerwijderd(0);
-                        echo "You shall not pass! Seems like StartTime is history. LastMinuteID" . $p->LastMinuteID . " will be marked as deleted. updating setBoolVerwijderd(1)<br>";
+                        $timeslot->setBoolVerwijderd(1);
+                        $timeslot->setDeleted("True");
+                            echo "<b>added record but - Seems like StartTime is history. LastMinuteID " . $p->LastMinuteID . " will be marked as deleted. updating setBoolVerwijderd(1)</b><br>";
+
                     endif;
                     $em = $this->getDoctrine()->getManager();
                     $em->persist($timeslot);
                     $em->flush();
                     echo "Excellent Work! LastMinuteID: " . $p->LastMinuteID . " added.. Since it's non-existent in db<br>";
                 else:
+                    if ($this->checkIfStarttimeIsPastFor($p->LastMinuteID)):
+                        $em = $this->getDoctrine()->getManager();
+                        $repo = $em->getRepository('AppBundle:Timeslots');
+                        $timeslot = $repo->findOneBy(array('lastminuteid' => $p->LastMinuteID));
+                        if (strtotime($this->checkStartTimeFor($p->LastMinuteID)[0]['starttime']) < time()):
+                            $timeslot->setBoolVerwijderd(1);
+                            $timeslot->setDeleted("True");
+                            if ($this->checkStartTimeFor($p->LastMinuteID)[0]['deleted'] != "True" || $this->checkStartTimeFor($p->LastMinuteID)[0]['bool_verwijderd'] != 1):
+                                $timeslot->setDeleted('True');
+                                $timeslot->setBoolVerwijderd(1);
+                                echo "<b>Seems like StartTime is history. LastMinuteID " . $p->LastMinuteID . " will be marked as deleted. updating setBoolVerwijderd(1)</b><br>";
+                            endif;
+                        endif;
+                        $timeslot->setLastminuteid($p->LastMinuteID);
+                        $timeslot->setCompanyid($p->CompanyID);
+                        $timeslot->setStarttime($p->StartTime);
+                        $timeslot->setEndtime($p->EndTime);
+                        $timeslot->setDescription($p->Description);
+                        $timeslot->setCost($p->Cost);
+                        $timeslot->setOriginalcost($p->OriginalCost);
+                        $timeslot->setInsertdate($p->InsertDate);
+                        $timeslot->setReservationurl($p->ReservationURL);
+                        $em->persist($timeslot);
+                        $em->flush();
+                    endif;
                     if ($this->checkIfTimeslotChangedFor($p->LastMinuteID, $p->InsertDate)):
                         $em = $this->getDoctrine()->getManager();
                         $repo = $em->getRepository('AppBundle:Timeslots');
@@ -81,7 +112,6 @@ class DefaultController extends Controller
                         $timeslot->setCost($p->Cost);
                         $timeslot->setOriginalcost($p->OriginalCost);
                         $timeslot->setInsertdate($p->InsertDate);
-                        $timeslot->setDeleted($p->Deleted);
                         $timeslot->setReservationurl($p->ReservationURL);
                         $em->flush();
                         echo "Brilliant! Looks like LastMinuteID: " . $p->LastMinuteID . " is changed (insertdate is diffrent). updating values..<br>";
@@ -100,7 +130,7 @@ class DefaultController extends Controller
         //assuming InsertDate is date when timeslot is added
         $repository = $this->getDoctrine()->getRepository('AppBundle:Timeslots');
         $query = $repository->createQueryBuilder('t')
-            ->select('t.lastminuteid,t.insertdate')
+            ->select('t.lastminuteid,t.insertdate,t.starttime')
             ->where('t.lastminuteid = :lastminute_id')
             ->setParameters(['lastminute_id' => $xml_lastmin_id])
             ->getQuery();
@@ -110,6 +140,24 @@ class DefaultController extends Controller
         else:
             return false; //it didn't change
         endif;
+    }
+
+    public function checkIfStarttimeIsPastFor($xml_lastmin_id)
+    {
+        //assuming InsertDate is date when timeslot is added
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Timeslots');
+        $query = $repository->createQueryBuilder('t')
+            ->select('t.lastminuteid,t.insertdate,t.starttime')
+            ->where('t.lastminuteid = :lastminute_id')
+            ->setParameters(['lastminute_id' => $xml_lastmin_id])
+            ->getQuery();
+        $result = $query->getScalarResult();
+        if ($result[0]['starttime'] < time()):
+            return true; //it changed
+        else:
+            return true;
+        endif;
+
     }
 
     public function checkIfTimesloExist($id = -1)
@@ -152,6 +200,18 @@ class DefaultController extends Controller
     public function getXmlUrl()
     {
         return $this->xml_url;
+    }
+
+    public function checkStartTimeFor($id)
+    {
+        $repository = $this->getDoctrine()->getRepository('AppBundle:Timeslots');
+        $query = $repository->createQueryBuilder('p')
+            ->select('p.lastminuteid, p.starttime,p.deleted,p.bool_verwijderd')
+            ->where('p.lastminuteid = :lastminuteid')
+            ->setParameters(['lastminuteid' => $id])
+            ->setMaxResults(1)
+            ->getQuery();
+        return $query->getResult();
     }
 
 }
